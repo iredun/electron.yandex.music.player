c726ae7c18af884ff7897c42b2b4f48971f6c10f
Ivan Redun <redunivan@yandex.ru>
2018-09-06 18:48:19 +0300

    Track notification

--- renderer.js
+++ renderer.js
@@ -1,14 +1,41 @@
 const {ipcRenderer} = require('electron')
 const webview = document.querySelector('webview')
 
+function getNextTrackInfo() {
+    webview.executeJavaScript('externalAPI.getNextTrack();', false, (track) => {
+        let myNotification = new Notification(track.title, {
+            body: track.artists[0].title
+        })
+    })
+}
+
+function getPrevTrackInfo() {
+    webview.executeJavaScript('externalAPI.getPrevTrack();', false, (track) => {
+        let myNotification = new Notification(track.title, {
+            body: track.artists[0].title
+        })
+    })
+}
+
+function getTrackInfo() {
+    webview.executeJavaScript('externalAPI.getTrack();', false, (track) => {
+        let myNotification = new Notification(track.title, {
+            body: track.artists[0].title
+        })
+    })
+}
+
 ipcRenderer.on('player', (event, arg) => {
     if (arg == 'play_pause') {
-        webview.executeJavaScript('externalAPI.togglePause();');        
+        webview.executeJavaScript('externalAPI.togglePause();');
+        getTrackInfo();   
     }
     if (arg == 'back') {
-        webview.executeJavaScript('externalAPI.prev();');
+        getPrevTrackInfo();
+        webview.executeJavaScript('externalAPI.prev();');        
     }
     if (arg == 'forward') {
-        webview.executeJavaScript('externalAPI.next();');
+        getNextTrackInfo();
+        webview.executeJavaScript('externalAPI.next();');        
     } 
 })
